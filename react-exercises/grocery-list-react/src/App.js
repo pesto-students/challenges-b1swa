c8d0b5eecc4b9fc485f39f6c5402119b6766d117
Biswajit Oraon <biswajitoraon19@gmail.com>
2020-10-22 15:34:05 +0530

    refactor: Implement object for state

--- react-exercises/grocery-list-react/src/App.js
+++ react-exercises/grocery-list-react/src/App.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const fakeData = [{name: "apple", value: 1, completed: false}, {name: "banana", value: 1, completed: false}, {name: "mango", value: 1, completed: false}]
+
 function GroceryList(props) {
   return(
-    <div>
-      {console.log(props)}
-      {props.groceryList.map(grocery => <GroceryItem {...grocery} key={grocery.name}/>)}
+    <div className="groceryList">
+      {props.groceryList.map(item => (<GroceryItem {...item} key={item.name}/>))}
     </div>
   );
 }
 
 function GroceryItem(props) {
+  const doneTask = (event) => {
+  
+  }
   return(
-    <div>
+    <div className="groceryItem" id={props.name}>
       {props.name}
+      <button onClick={doneTask} value={props.name}>Done</button>
+      <button>Delete</button>
     </div>
   );
 }
@@ -27,25 +33,48 @@ function Form(props) {
     props.onSubmit(groceryItem);
     setGroceryItem('');
   }
+
+  const clearItems = () => {
+    props.onClearItems();
+  }
+
   return(
-    <form onSubmit={handleSubmit}>
+    <form>
       <input type="text" placeholder="Grocery Item" value={groceryItem} onChange={event => setGroceryItem(event.target.value)} required></input>
-      <button>Add Item</button>
+      <button onClick={handleSubmit}>Add Item</button>
+      <button onClick={clearItems}>Clear</button>
     </form>
   );
 }
 
 
 function App() {
-  const [groceryList, setGroceryList] = useState([]);
+  const [groceryList, setGroceryList] = useState(fakeData);
   const addItem = (item) => {
-    setGroceryList(prevState => ([...prevState, {"name": item}]));
+    setGroceryList(prevState => (
+      [...prevState, {name: item, value: 1, completed: false}]
+    ));
   }
+  const deleteItem = (item) => {
+    setGroceryList(prevState => (
+      prevState.find(item => item['name'] === item)
+    ));
+  }
+
+  const clearAllItems = () => {
+    setGroceryList([]);
+  }
+
+  const toggleComplete = (item) => {
+    setGroceryList(prevState => (prevState))
+  }
+
+
   return (
     <div>
       <h1>GROCERY LIST</h1>
       <GroceryList groceryList= {groceryList}/>
-      <Form onSubmit={addItem}></Form>
+      <Form onSubmit={addItem} onClearItems={clearAllItems}></Form>
     </div>
   );
 }
